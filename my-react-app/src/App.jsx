96de445836d1cae0f1bd190725afc26410932bd6
Lidia Parkhomova <slipinuk@gmail.com>
2024-12-15 14:29:28 +0100

    template

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,7 +9,6 @@ function App() {
       <div>
         <h1>My first React Project</h1>
       </div>
-
     </>
   )
 }
